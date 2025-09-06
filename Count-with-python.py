8c914d37b7e92fb0be1f8a782998b27a8d50661a
SebastianP <sebastian.petersson@iths.se>
2025-09-06 12:29:53 +0200

    Uppgift 2

--- Count-with-python.py
+++ Count-with-python.py
@@ -1,31 +1,45 @@
 #ÖVNINGSUPPGIFTER - RÄKNING MED PYTHON
 
 # A right  angled triangle has the catheti: a = 3 and b = 4 length units. 
-# Compute the hypothenuse of the triangle. (*)
+# Compute the hypothenuse of the triangle. (*) Första Lösning
+
 import math
-#a = 3
-#b = 4
-#print("Längden av hypotenusan är:", math.sqrt(a**2 + b**2))
 
+print("Uppgift 1a")
+#a = 3.0
+#b = 4.0
+#print(f"Längden av hypotenusan är", math.sqrt(a**2 + b**2))
 
-# A right angled triangle has hypothenuse c = 7.0 and a cathetus a = 5.0 length units. Compute the other cathetus and round to one decimal. (*)
-#a = 5.0
-#c =87.0
-#b = math.sqrt(c**2 - a**2)
-#print(f"Längden av den andra kateten är: {b:.3f}")
+#Annan version, beräkna_hypotenusa definieras som en funktion och använder math.hypot (inbyggd funktion) för att räkna ut hypotenusan. 
+def beräkna_hypotenusa (a: float, b: float) -> float:
+    return math.hypot(a, b)
 
+a = 3.0
+b = 4.0
+hypotenusa = beräkna_hypotenusa(a, b)
 
-#a = 3
-#b = 4
-#print("Längden av hypotenusan är:", math.sqrt(a**2 + b**2))
+print(f"Längden av hypotenusan är {hypotenusa:.1f} cm.")
 
+#__________________________________
+print("Uppgift 1b")
 
-#Jag ska nu räkna ut längden på den ena kateten om den andra katetens längd är 6 och hypotenusan är 11.
+#Uppgift 2: b)   A right angled triangle has hypothenuse c = 7.0 and a cathetus a = 5.0 length units. 
+# Compute the other cathetus and round to one decimal. (*)
 
-a = 6
-c = 11
-b = math.sqrt(c**2 - a**2)
-print(f"Längden av den andra kateten är: {b:.2f}")
+a = 5.0
+c = 7.0
+b = math.sqrt(c**2-a**2)
 
-#Svar: Längden av den andra kateten är: 8.06
+print(f"Längden av den andra katetern är: {b:.1f}")
 
+#__________________________________
+print("Uppgift 2")
+#2. Classification accuracy (*)
+#A machine learning algorithm has been trained to predict whether or not it would rain the next day. 
+# Out of 365 predictions, it got 300 correct, compute the accuracy of this model.
+
+correct_predictions = 300
+total_predictions = 365
+accuracy = (correct_predictions/total_predictions)
+
+print(f"Träffsäkerheten av ML-algoritmen är: {accuracy:.2f}")
