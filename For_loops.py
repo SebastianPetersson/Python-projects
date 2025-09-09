0b1097d9abba788499bcf1f4ef892ba7de4423eb
SebastianP <sebastian.petersson@iths.se>
2025-09-09 11:43:04 +0200

    Första egna program

--- For_loops.py
+++ For_loops.py
@@ -1,9 +1,19 @@
-#for i in range(-10, 11, 2):
-#    print(i, end=' ')
+#FÖRSTA EGENSKAPADE PROGRAMMET:
 
-sum = 0
+import random as rnd
 
-for tal in range(1, 101, 3):
-    sum +=tal
+antal_av_tal = 0
+antal_kast = int(input("Skriv hur många kast du vill göra med tärningen: "))
+antal_sidor = int(input("Hur många sidor vill du att tärningen ska ha?: "))
+valt_tal = int(input(f"Vilken siffra vill du jag ska räkna antalet av, mellan 1-{antal_sidor}?: "))
 
-print(f"Summan av talen 1-100 är {sum}.")
+
+for i in range(antal_kast):
+    tärning=rnd.randint(1,antal_sidor)
+    if tärning == valt_tal:
+        antal_av_tal += 1
+
+procent = (antal_av_tal/antal_kast)*100
+
+print(f"Av {antal_kast} kast med en {antal_sidor}-sidig tärning blev det {antal_av_tal} {valt_tal}:or.")
+print(f"Sannolikheten för en sexa är ca {procent:.2f}%.")
